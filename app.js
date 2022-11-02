c0f65fbf3cd306c28d1299a24aa17c7a1a224524
Syam kumar Allaparthi <syamkumar.allaparthi@gmail.com>
2022-11-01 19:40:53 -0500

    Updated Gridbuild files

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var fuelRouter = require('./routes/fuel');
+var gridbuildRouter = require('./routes/gridbuild');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/fuel', fuelRouter);
+app.use('/gridbuild', gridbuildRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
